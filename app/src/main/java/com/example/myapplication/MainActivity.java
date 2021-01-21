8cddcb6bcb84878b14ccd72fd6f30cabf0119d68
Anh Nguyen <nguye3_anh@bentley.edu>
2021-01-20 23:29:24 -0500

    Second Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //connect UI to Activity
+
+        Toast.makeText(this, "double foobar", Toast.LENGTH_LONG).show();
+
+        Log.e("Pepe","double foobar"); //write error to log
+
+
     }
 }
